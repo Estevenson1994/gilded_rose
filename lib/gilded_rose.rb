5baf20588ac1f821abc861b314b718d975ca1be2
Erin Stevenson <estevenson_94@live.com>
2019-03-16 16:28:13 +0000

    Back stage passes increases in value of 3 if there are between 0 and 5 days left

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -15,6 +15,8 @@ class GildedRose
           item.quality += 1
         elsif item.sell_in > 5
           item.quality += 2
+        elsif item.sell_in > 0
+          item.quality += 3
         end
       end
 
