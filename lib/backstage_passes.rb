cc005e4d90dc6114c0cd25bc86030c7c7f7e8ebe
Erin Stevenson <estevenson_94@live.com>
2019-03-17 13:03:11 +0000

    Encapsulate methods on backstagepasses module

--- lib/backstage_passes.rb
+++ lib/backstage_passes.rb
@@ -2,12 +2,30 @@ module Backstage_passes
 
     
   def backstage_passes(item)
-    item.quality += 1
-    item.quality += 1 if item.sell_in < 11
-    item.quality += 1 if item.sell_in < 6
-    item.quality = 50 if item.quality > 50
+    add_one_to_quality(item)
+    add_one_to_quality(item) if less_than_11_days_to_sell(item)
+    add_one_to_quality(item) if less_than_6_days_to_sell(item)
+    dont_let_quality_go_above_50(item)
+    set_quality_to_0_if_passed_sell_in(item)
+    remove_one_day_from_sell_in(item)
+  end
+
+  private
+
+  def set_quality_to_0_if_passed_sell_in(item)
     item.quality = 0 if item.sell_in <= 0
-    item.sell_in -= 1
+  end
+
+  def add_one_to_quality(item)
+    item.quality += 1
+  end
+
+  def less_than_11_days_to_sell(item)
+    item.sell_in < 11
+  end
+
+  def less_than_6_days_to_sell(item)
+    item.sell_in < 6
   end
 
 end
