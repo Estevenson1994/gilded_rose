18952f55e544d55653e8c93d9fb70cc6f99407b1
Erin Stevenson <estevenson_94@live.com>
2019-03-17 16:45:41 +0000

    encapsulated conjured item methods

--- lib/conjured_item.rb
+++ lib/conjured_item.rb
@@ -1,12 +1,26 @@
 module Conjured_item
 
     def update_conjured_item(item)
-        if item.sell_in > 0 && item.quality > 0
-          item.quality -= 2
-        elsif item.quality < 0
-          item.quality = 0
-        end
+      remove_two_from_quality(item) if item.sell_in > 0 && quality_is_greater_than_0(item)
+      set_quality_to_0(item) if quality_is_less_than_0(item) 
+    end
 
+    private
+
+    def remove_two_from_quality(item)
+      item.quality -= 2
+    end
+
+    def quality_is_less_than_0(item)
+      item.quality < 0
+    end
+
+    def set_quality_to_0(item)
+      item.quality = 0
+    end
+
+    def quality_is_greater_than_0(item)
+      item.quality > 0
     end
 
 end
